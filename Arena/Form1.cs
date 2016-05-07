da9df59a6bf4fd9344749f98686a3b95a23f17c2
Harrison Orders <newrasputin1@gmail.com>
2016-05-07 16:54:54 -0700

    Added a pistol

--- Arena/Form1.cs
+++ Arena/Form1.cs
@@ -16,5 +16,33 @@ namespace Arena
         {
             InitializeComponent();
         }
+        static Random r = new Random();
+
+        Dictionary<string, int> pistol = new Dictionary<string, int>()
+        {
+            {"damage", r.Next(10, 15)}
+        };
+
+        public class Fighter
+        {
+            public int health;
+
+            public void setHealth()
+            {
+                health = r.Next(90, 141);
+            }
+        }
+
+        public void generateFighter()
+        {
+            Fighter fighter1 = new Fighter();
+            fighter1.setHealth();
+            textBox1.Text = "An enemy aproaches with" + fighter1.health.ToString() + " hp and toting a pistol that deals out " + pistol["damage"] + " damage";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            generateFighter();
+        }
     }
 }
